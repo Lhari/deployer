590523907712805e58ed8c957835ddb76b0e3986
Lhari <kashan7@live.dk>
2016-09-13 10:08:48 +0200

    added wp

--- build/mkcomposer.sh
+++ build/mkcomposer.sh
@@ -25,6 +25,10 @@ cat > composer.json <<- _EOF_
                 "name": "$PROJECTNAME",
                 "type": "vcs",
                 "url": "git@github.com:klean/$GITNAME.git"
+        },
+        {
+            "type": "composer",
+            "url": "http://wpackagist.org"
         }
     },
     "require": {
@@ -34,9 +38,13 @@ cat > composer.json <<- _EOF_
         "connect20/cadence_fbpixel": "*",
         "connect20/chapagain_googletagmanager": "0.1.0",
         "aoepeople/aoe_scheduler": "1.*",
-        "$PROJECTNAME": "dev-$BRANCH"
+        "$PROJECTNAME": "dev-$BRANCH",
+        "php": ">=5.4",
+        "johnpbloch/wordpress": "4.*",
+        "fancyguy/wordpress-monolog": "dev-master",
     },
     "extra": {
+        "wordpress-install-dir": "wp",
         "magento-deploystrategy": "copy",
         "magento-root-dir": "public_html__new",
         "auto-append-gitignore": true,
