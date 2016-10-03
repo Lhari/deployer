14a199ce8b266b84f892f3511a6cd2f0579a8d83
Lhari <kashan7@live.dk>
2016-10-03 14:21:50 +0200

    update composer file to have "settings" for wp

--- build/mkcomposer.sh
+++ build/mkcomposer.sh
@@ -1,7 +1,8 @@
 . vars/settings.sh
 
-cat > composer.json <<- _EOF_
+# Old Extensions, removed for various reasons
 
+cat > composer.json <<- _EOF_
 {
     "name": "Klean/CI Deployer",
     "description": "first iteration of CI deployer files",
@@ -26,43 +27,62 @@ cat > composer.json <<- _EOF_
                 "type": "vcs",
                 "url": "git@github.com:klean/$GITNAME.git"
         },
+
+_EOF_
+
+if [[ $ISWP = true ]]; then
+cat << EOT >> composer.json
+        "wordpress": {
+            "type": "composer",
+            "url": "http://wpackagist.org"
+        },
+EOT
+fi
+
+cat << EOT >> composer.json
         "block": {
             "name": "blocks",
             "type": "vcs",
             "url": "git@github.com:klean/blocks.git"
-        },
-        "wordpress": {
-            "type": "composer",
-            "url": "http://wpackagist.org"
         }
     },
     "require": {
         "magento-hackathon/magento-composer-installer": "*",
         "aydin-hassan/magento-core-composer-installer": "*",
         "firegento/magento": "1.9.2.*",
-        "connect20/cadence_fbpixel": "*",
-        "connect20/chapagain_googletagmanager": "0.1.0",
-        "connect20/aromicon_google_universal_analytics": "*",
-        "aoepeople/aoe_scheduler": "1.*",
         "$PROJECTNAME": "dev-$BRANCH",
-        "blocks": "dev-master",
+        "connect20/chapagain_googletagmanager": "0.1.0",
+EOT
+
+. vars/serverExtensions.sh
+
+if [[ $ISWP = true ]]; then
+cat << EOT >> composer.json
         "php": ">=5.4",
         "johnpbloch/wordpress": "4.*",
         "wpackagist-plugin/user-role-editor": "*",
         "wpackagist-plugin/login-redirect-url": "*",
         "wpackagist-plugin/simple-seo-pack": "*",
-        "wpackagist-plugin/akismet": "*"
+        "wpackagist-plugin/akismet": "*",
+EOT
+fi
+
+cat << EOT >> composer.json
+        "blocks": "dev-master"
     },
     "extra": {
+EOT
+
+if [[ $ISWP = true ]]; then
+cat << EOT >> composer.json
         "wordpress-install-dir": "public_html__new/wp/",
+EOT
+fi
+
+cat << EOT >> composer.json
         "magento-deploystrategy": "copy",
         "magento-root-dir": "public_html__new",
         "auto-append-gitignore": true,
         "magento-force": true
     }
 }
-
-_EOF_
-
-# Old Extensions, removed for various reasons
-# "connect20/chapagain_googletagmanager": "0.1.0",
